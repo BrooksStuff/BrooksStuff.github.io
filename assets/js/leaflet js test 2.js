a1743a120a339eb26c6571c16e81b856098616f4
BrooksStuff <josephbrooks509@gmail.com>
2024-06-04 17:46:05 -0500

    Website updates.

--- assets/js/leaflet js test 2.js
+++ assets/js/leaflet js test 2.js
@@ -1,8 +1,9 @@
-const map = L.map('mapid').setView([51.505, -0.09], 13);
+const map = L.map('mapid').setView([31.2564, -91.9613], 7);
     
     //tile location specification
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png', {
+    minZoom: 7,
+    maxZoom: 18,
+    attribution: '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
 }).addTo(map);
