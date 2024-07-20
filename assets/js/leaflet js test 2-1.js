36713a1e4ebae2e8c44fe932968a3d28380ba39d
BrooksStuff <josephbrooks509@gmail.com>
2024-07-20 15:49:42 -0500

    Website updates.

--- assets/js/leaflet js test 2-1.js
+++ assets/js/leaflet js test 2-1.js
@@ -1,3 +1,8 @@
+//notes - MARKERS OUT OF DATE - SEVERAL MARKERS MUST be ADDED
+
+
+
+
 const map = L.map('mapid').setView([31.2564, -91.9613], 7);
     
     //tile location specification
@@ -10,69 +15,73 @@ const map = L.map('mapid').setView([31.2564, -91.9613], 7);
 
 
 
-
+    map.on('zoomend', function() {
+        var currentZoom = map.getZoom();
+        FountainebleauRuinsIcon5min.seticonSize(currentZoom);
+      });
 
 
 
 //icon definitions
 var FountainebleauRuinsIcon5min = L.icon({
-    iconUrl: 'FountainebleauBrickFactoryRuins5min.png',
+    iconUrl: 'Fountainebleau_radial.png',
 
     iconSize:     [100, 100], // size of the icon
     popupAnchor:     [0, -50], // origin of popup
+   
 });
 
 var AlligatorMarshIcon5min = L.icon({
-    iconUrl: 'AlligatorMarsh5min.png',
+    iconUrl: 'AlligatorMarsh_radial.png',
 
     iconSize:     [100, 100], // size of the icon
     popupAnchor:     [0, -50], // origin of popup
 });
 
 var BayouLibertyRuinsIcon5min = L.icon({
-    iconUrl: 'BayouLibertyRuins5min.png',
+    iconUrl: 'BayouLiberty_angled.png',
 
     iconSize:     [100, 100], // size of the icon
     popupAnchor:     [0, -50], // origin of popup
 });
 
 var BigBranchNWRIcon5min = L.icon({
-    iconUrl: 'BigBranchNWRIcon5min.png',
+    iconUrl: 'BigBranch_radial.png',
 
     iconSize:     [100, 100], // size of the icon
     popupAnchor:     [0, -50], // origin of popup
 });
 
 var FirstStCharlesIcon5min = L.icon({
-    iconUrl: 'FirstAndStCharles5min.png',
+    iconUrl: 'FirstStCharles_radial.png',
 
     iconSize:     [100, 100], // size of the icon
     popupAnchor:     [0, -50], // origin of popup
 });
 
 var MulattoBendCemeteryIcon5min = L.icon({
-    iconUrl: 'MulattoBendCemetery5min.png',
+    iconUrl: 'MulattoBend_radial.png',
 
     iconSize:     [100, 100], // size of the icon
     popupAnchor:     [0, -50], // origin of popup
 });
 
 var AlmaCemeteryIcon5min = L.icon({
-    iconUrl: 'AlmaCemetery5min.png',
+    iconUrl: 'Alma_radial.png',
 
     iconSize:     [100, 100], // size of the icon
     popupAnchor:     [0, -50], // origin of popup
 });
 
 var BucktownBirdNestIcon5min = L.icon({
-    iconUrl: 'BucktownBirdNest5min.png',
+    iconUrl: 'BucktownBirdsNest_radial.png',
 
     iconSize:     [100, 100], // size of the icon
     popupAnchor:     [0, -50], // origin of popup
 });
 
 var CityParkAirfieldIcon5min = L.icon({
-    iconUrl: 'CityParkAirfield5min.png',
+    iconUrl: 'AirField_radial.png',
 
     iconSize:     [100, 100], // size of the icon
     popupAnchor:     [0, -50], // origin of popup
@@ -86,42 +95,42 @@ var CouturieForestTrailIcon5min = L.icon({
 });
         
 var PumpingStation1Icon5min = L.icon({
-    iconUrl: 'PumpingStation1_5min.png',
+    iconUrl: 'PumpingStation1_radial.png',
 
     iconSize:     [100, 100], // size of the icon
     popupAnchor:     [0, -50], // origin of popup
 });    
 
 var ChalmetteWetlandIcon5min = L.icon({
-    iconUrl: 'ChalmetteWetland5min.png',
+    iconUrl: 'ChalmetteWetland_radial.png',
 
     iconSize:     [100, 100], // size of the icon
     popupAnchor:     [0, -50], // origin of popup
 });  
 
 var JacksonSquareIcon5min = L.icon({
-    iconUrl: 'JacksonSquare5min.png',
+    iconUrl: 'JacksonSquare_radial.png',
 
     iconSize:     [100, 100], // size of the icon
     popupAnchor:     [0, -50], // origin of popup
 });  
 
 var BrassBandIcon5min = L.icon({
-    iconUrl: 'BrassBand5min.png',
+    iconUrl: 'BrassBand_radial.png',
 
     iconSize:     [100, 100], // size of the icon
     popupAnchor:     [0, -50], // origin of popup
 });  
 
 var BourbonStAnneIcon5min = L.icon({
-    iconUrl: 'BourbonStAnne5min.png',
+    iconUrl: 'BourbonStAnn_radial.png',
 
     iconSize:     [100, 100], // size of the icon
     popupAnchor:     [0, -50], // origin of popup
 });  
 
 var BourbonToulouseIcon5min = L.icon({
-    iconUrl: 'BourbonToulouse5min.png',
+    iconUrl: 'BourbonToulouse_radial.png',
 
     iconSize:     [100, 100], // size of the icon
     popupAnchor:     [0, -50], // origin of popup
@@ -130,66 +139,67 @@ var BourbonToulouseIcon5min = L.icon({
 
 
 //marker definitions
-L.marker([30.337159, -90.038018],{icon:FountainebleauRuinsIcon5min, opacity: 0.53, riseOnHover: 1}).addTo(map)
+L.marker([30.337159, -90.038018],{icon:FountainebleauRuinsIcon5min},{opacity:0.5}).addTo(map)
     .bindPopup('Marigny/Fountainebleau Sugar Mill Ruins')
     .openPopup();
 
-L.marker([30.333147, -90.039685],{icon:AlligatorMarshIcon5min, opacity: 0.53, riseOnHover: 1}).addTo(map)
+L.marker([30.333147, -90.039685],{icon:AlligatorMarshIcon5min}).addTo(map)
     .bindPopup('Alligator Marsh Boardwalk')
     .openPopup();
 
-L.marker([30.292726, -89.826879],{icon:BayouLibertyRuinsIcon5min, opacity: 0.53, riseOnHover: 1}).addTo(map)
+L.marker([30.292726, -89.826879],{icon:BayouLibertyRuinsIcon5min}).addTo(map)
     .bindPopup('Bayou Liberty Ruins')
     .openPopup();
 
-L.marker([30.268734, -89.955868],{icon:BigBranchNWRIcon5min, opacity: 0.53, riseOnHover: 1}).addTo(map)
+L.marker([30.268734, -89.955868],{icon:BigBranchNWRIcon5min}).addTo(map)
     .bindPopup('Big Branch NWR Boat Launch')
     .openPopup();
 
-L.marker([29.932598, -90.082792],{icon:FirstStCharlesIcon5min, opacity: 0.53, riseOnHover: 1}).addTo(map)
+L.marker([29.932598, -90.082792],{icon:FirstStCharlesIcon5min}).addTo(map)
     .bindPopup('First Street and St. Charles Ave Streetcar Stop')
     .openPopup();
 
-L.marker([30.503109, -91.228009],{icon:MulattoBendCemeteryIcon5min, opacity: 0.53, riseOnHover: 1}).addTo(map)
+L.marker([30.503109, -91.228009],{icon:MulattoBendCemeteryIcon5min}).addTo(map)
     .bindPopup('Mulatto Bend Cemetery')
     .openPopup();
 
-L.marker([30.597367, -91.383419],{icon:AlmaCemeteryIcon5min, opacity: 0.53, riseOnHover: 1}).addTo(map)
+L.marker([30.597367, -91.383419],{icon:AlmaCemeteryIcon5min}).addTo(map)
     .bindPopup('Alma Cemetery')
     .openPopup();
 
-L.marker([30.021323, -90.126837],{icon:BucktownBirdNestIcon5min, opacity: 0.53, riseOnHover: 1}).addTo(map)
+L.marker([30.021323, -90.126837],{icon:BucktownBirdNestIcon5min}).addTo(map)
     .bindPopup('Bucktown Bird Nest')
     .openPopup();
 
-L.marker([30.004078,-90.092503],{icon:CityParkAirfieldIcon5min, opacity: 0.53, riseOnHover: 1}).addTo(map)
+L.marker([30.004078,-90.092503],{icon:CityParkAirfieldIcon5min}).addTo(map)
     .bindPopup('City Park Model Airfield')
     .openPopup();
 
-L.marker([30.007084,-90.091961],{icon:CouturieForestTrailIcon5min, opacity: 0.53, riseOnHover: 1}).addTo(map)
+L.marker([30.007084,-90.091961],{icon:CouturieForestTrailIcon5min}).addTo(map)
     .bindPopup('Couturie Forest Trail')
     .openPopup();
 
-L.marker([29.965956, -89.975911],{icon:PumpingStation1Icon5min, opacity: 0.53, riseOnHover: 1}).addTo(map)
+L.marker([29.965956, -89.975911],{icon:PumpingStation1Icon5min}).addTo(map)
     .bindPopup('St. Bernard Parish Drainage Pumping Station')
     .openPopup();
 
-L.marker([29.965956, -89.975911],{icon:ChalmetteWetlandIcon5min, opacity: 0.53, riseOnHover: 1}).addTo(map)
+L.marker([29.965956, -89.975911],{icon:ChalmetteWetlandIcon5min}).addTo(map)
     .bindPopup('Chalmette Wetland')
     .openPopup();
 
-L.marker([29.957158, -90.063308],{icon:JacksonSquareIcon5min, opacity: 0.53, riseOnHover: 1}).addTo(map)
+L.marker([29.957158, -90.063308],{icon:JacksonSquareIcon5min}).addTo(map)
     .bindPopup('Jackson Square')
     .openPopup();
 
-L.marker([29.957958, -90.064905],{icon:BrassBandIcon5min, opacity: 0.53, riseOnHover: 1}).addTo(map)
+L.marker([29.957958, -90.064905],{icon:BrassBandIcon5min}).addTo(map)
     .bindPopup('Brass Band at the Corner of Royal Street and St. Peter Street')
     .openPopup();
 
-L.marker([29.959421, -90.064883],{icon:BourbonStAnneIcon5min, opacity: 0.53, riseOnHover: 1}).addTo(map)
+L.marker([29.959421, -90.064883],{icon:BourbonStAnneIcon5min}).addTo(map)
     .bindPopup('Bourbon Street and St. Anne Street')
     .openPopup();
 
-L.marker([29.958274, -90.065975],{icon:BourbonToulouseIcon5min, opacity: 0.53, riseOnHover: 1}).addTo(map)
+L.marker([29.958274, -90.065975],{icon:BourbonToulouseIcon5min}).addTo(map)
     .bindPopup('Bourbon Street')
     .openPopup();
+
